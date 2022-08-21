eb651a8f5395a6a53248286091d33d44051f4f61
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-21 20:57:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,26 @@
-
-ls /bin
+# kakathic
 
 Likk="$GITHUB_WORKSPACE"
 
 Taive(){ curl -# -L "$1" -o "$2"; }
 Xem(){ curl -s -L -G "$@"; }
-Getpro () { grep -m1 "$1=" $Likk/Zom_Custom.md | cut -d = -f2; }
+Getpro () { grep -m1 "$1=" $Likk/Zom_custom.md | cut -d = -f2; }
+
+ListTM="tmp
+Unpack
+Repapk
+Unzip
+"
+
+for Vak in $ListTM; do
+mkdir -p $Vak
+done
+
+Taive "$(Getpro Http)" "$Likk/rom.zip"
+
+unzip -qo "$Likk/rom.zip" -d "$Likk/Unzip"
+
+
+
+
 
