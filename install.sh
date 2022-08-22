e2b4b989777f96c1365ffbf335a1907a4e834fac
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-22 23:07:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,12 +25,10 @@ sudo apt-get update >/dev/null
 sudo apt-get install zstd binutils e2fsprogs erofs-utils >/dev/null
 pip3 install protobuf bsdiff4 six crypto construct google docopt pycryptodome >/dev/null
 
-pip3 --help
-
 echo "pycryptodome
 docopt
 protobuf<=3.20.1" > requirements.txt
-pip3 -r requirements.txt
+pip3 install -r requirements.txt
 
 
 
