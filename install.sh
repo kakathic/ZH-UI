2f3b19a5cd44681b03ecc013220d86dfce40ffa4
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-22 23:02:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,10 +25,14 @@ sudo apt-get update >/dev/null
 sudo apt-get install zstd binutils e2fsprogs erofs-utils >/dev/null
 pip3 install protobuf bsdiff4 six crypto construct google docopt pycryptodome >/dev/null
 
+pip3 --help
+
 echo "pycryptodome
 docopt
 protobuf<=3.20.1" > requirements.txt
 pip3 -r requirements.txt
+
+
 
 Taive "$(Getpro Http)" "$Likk/rom.zip"
 unzip -qo "$Likk/rom.zip" -d "$Likk/Unzip"
