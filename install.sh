7e24dd34447d4556269f3d03a20a1221dd6f75fe
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-22 21:37:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ uname -a
 echo ::set-output name=uname::$(uname -a)
 
 sudo apt-get update >/dev/null
-sudo apt-get install -y zstd binutils e2fsprogs >/dev/null
+sudo apt-get install -y zstd binutils e2fsprogs erofs-utils >/dev/null
 pip3 install protobuf bsdiff4 six crypto construct google docopt pycryptodome >/dev/null
 
 #Taive "$(Getpro Http)" "$Likk/rom.zip"
