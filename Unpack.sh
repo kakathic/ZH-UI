d54cda09c3139c5558fad8cf418d88f58bf8db1f
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-23 00:05:16 +0700

    Update Unpack.sh

--- Unpack.sh
+++ Unpack.sh
@@ -1,3 +1,5 @@
+Xemb(){ ls -l $1 | awk '{print $5}'; }
+
 echo "- Bung payload.bin"
 
 python3 $Likk/Libpy/payload_dumper.py $Likk/Unzip/payload.bin --out $Likk/Payload > /dev/null 
@@ -5,3 +7,5 @@ python3 $Likk/Libpy/payload_dumper.py $Likk/Unzip/payload.bin --out $Likk/Payloa
 for kdbfnd in system.img vendor.img product.img odm.img system_ext.img; do
 mv -f $Likk/Payload/$kdbfnd $Likk/Super
 done
+
+$(Xemb $Likk/Super/system.img)
