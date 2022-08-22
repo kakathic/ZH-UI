a4721f41d76bb8b90afe06677e00da7f5c0138e7
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-22 22:15:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,31 +25,10 @@ sudo apt-get update >/dev/null
 sudo apt-get install zstd binutils e2fsprogs erofs-utils >/dev/null
 pip3 install protobuf bsdiff4 six crypto construct google docopt pycryptodome >/dev/null
 
-#Taive "$(Getpro Http)" "$Likk/rom.zip"
-#unzip -qo "$Likk/rom.zip" -d "$Likk/Unzip"
-#ls "$Likk/Unzip"
+Taive "$(Getpro Http)" "$Likk/rom.zip"
+unzip -qo "$Likk/rom.zip" -d "$Likk/Unzip"
+ls "$Likk/Unzip"
 
 . $Likk/Unpack.sh
 . $Likk/Mod.sh
 . $Likk/Repack.sh
-
-fsck.erofs | tee -a $Likk/bin.txt
-lpunpack | tee -a $Likk/bin.txt
-lpmake | tee -a $Likk/bin.txt
-lpadd | tee -a $Likk/bin.txt
-lpdump | tee -a $Likk/bin.txt
-lpflash | tee -a $Likk/bin.txt
-make_ext4fs | tee -a $Likk/bin.txt
-mkfs.erofs | tee -a $Likk/bin.txt
-resize2fs | tee -a $Likk/bin.txt
-tune2fs | tee -a $Likk/bin.txt
-zstd | tee -a $Likk/bin.txt
-brotli | tee -a $Likk/bin.txt
-w_e2fsdroid | tee -a $Likk/bin.txt
-w_lpmake | tee -a $Likk/bin.txt
-w_mke2fs | tee -a $Likk/bin.txt
-
-ls /bin >> $Likk/bin.txt
-
-
-
