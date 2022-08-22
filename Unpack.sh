2401c6be9ee7321c5c4a17d4c9919ee4591a2185
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-22 23:18:47 +0700

    Update Unpack.sh

--- Unpack.sh
+++ Unpack.sh
@@ -1,3 +1,4 @@
 echo "- Bung payload.bin"
 
-python3 $Likk/Libpy/payload_dumper.py $Likk/Unzip/payload.bin --out $Likk/Payload
+python3 $Likk/Libpy/payload_dumper.py $Likk/Unzip/payload.bin --out $Likk/Payload >/dev/null 2>/dev/null
+
