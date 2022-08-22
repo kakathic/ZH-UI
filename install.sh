42486829c860302e7c508af0187355a785621793
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-22 15:33:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ sudo apt-get update >/dev/null
 sudo apt-get install -y zstd binutils e2fsprogs android-sdk-ext4-utils >/dev/null
 pip3 install protobuf bsdiff4 six crypto construct google docopt pycryptodome >/dev/null
 
-dnf install erofs-utils
+sudo pacman -S erofs-utils
 
 #Taive "$(Getpro Http)" "$Likk/rom.zip"
 #unzip -qo "$Likk/rom.zip" -d "$Likk/Unzip"
