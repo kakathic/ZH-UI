b92dcfaa3226eaf3ac69f78c65c407ea9e9ebb38
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-23 00:28:47 +0700

    Update Unpack.sh

--- Unpack.sh
+++ Unpack.sh
@@ -8,4 +8,11 @@ for kdbfnd in system.img vendor.img product.img odm.img system_ext.img; do
 mv -f $Likk/Payload/$kdbfnd $Likk/Super
 done
 
-$(Xemb $Likk/Super/system.img)
+system_size=$(Xemb $Likk/Super/system.img)
+system_ext_size=$(Xemb $Likk/Super/system_ext.img)
+vendor_size=$(Xemb $Likk/Super/vendor.img)
+product_size=$(Xemb $Likk/Super/product.img)
+odm_size=$(Xemb $Likk/Super/odm.img)
+# Tính tổng kích cỡ thư mục super 
+super_size=$(ls -l $Likk/Super | sed -n 1p | awk '{print int($2)}'); 
+
