ea6628d7f344200496df9499c37f8b434d1d7eab
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-24 17:37:23 +0700

    Update Unpack.sh

--- Unpack.sh
+++ Unpack.sh
@@ -4,6 +4,6 @@ echo "- Bung payload.bin"
 
 python3 $Likk/Libpy/payload_dumper.py $Likk/Unzip/payload.bin --out $Likk/Payload > /dev/null 
 
-for kdbfnd in system*.img vendor*.img product*.img odm*.img system_ext*.img; do
+for kdbfnd in system.img vendor.img product.img odm.img system_ext.img; do
 mv -f $Likk/Payload/$kdbfnd $Likk/Super
 done
