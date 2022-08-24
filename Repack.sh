2288e834933613cf231d6f998aa897ac4dd93e1c
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-24 22:38:25 +0700

    Update Repack.sh

--- Repack.sh
+++ Repack.sh
@@ -1,69 +1,55 @@
 # Loại phân vùng (1,2,3) 
-sokhe=1
+Sokhe=1
 
 # Tên nhóm
-nhom=main
+Nhom=main
 
 # Chế độ (none/readonly)
-chedo=none
+Chedo=none
 
 # Kích cỡ phân vùng super 8.5GB
-ssize=8.5
+Ssize=8.5
 
 # Tạo super.img
-taosuper() { lpmake -d "$super_raw_size" -s "$sokhe" -m 65536 -g "$nhom":"$super_size" --super-name super -p system:"$chedo":"$size_system":"$nhom" -i system=system.img -p system_ext:"$chedo":"$size_system_ext":"$nhom" -i system_ext=system_ext.img -p vendor:"$chedo":"$size_vendor":"$nhom" -i vendor=vendor.img -p product:"$chedo":"$size_product":"$nhom" -i product=product.img -p odm:"$chedo":"$size_odm":"$nhom" -i odm=odm.img -o $Likk/super.img; } 
+taosuper() { lpmake -d "$Ssuperr" -s "$Sokhe" -m 65536 -g "$Nhom":"$Ssuper" --super-name super -p system:"$Chedo":"$Ssystem":"$Nhom" -i system=system.img -p system_ext:"$Chedo":"$Ssystem_ext":"$Nhom" -i system_ext=system_ext.img -p vendor:"$Chedo":"$Svendor":"$Nhom" -i vendor=vendor.img -p product:"$Chedo":"$Sproduct":"$Nhom" -i product=product.img -p odm:"$Shedo":"$Sodm":"$Nhom" -i odm=odm.img -o $Likk/super.img; } 
 
 # Kiểm tra kích cỡ và tạo super.img 
+tongkichco() { Ssuper="$(ls -l $Likk/Super | sed -n 1p | awk '{print int($2)}')"; } 
+
+kichcosuper() { Ssuperr="$(awk "BEGIN {print int($Ssize*1024*1024*1024)}")"; } 
+
+kichco() { S${TEN}=$(wc -c < $Likk/Super/$TEN.img); } 
+
 giamthieu() { resize2fs -f -M $Likk/Super/$TEN.img > /dev/null 2>&1 && resize2fs -f -M $Likk/Super/$TEN.img > /dev/null 2>&1; } 
 
-tangkichco() { resize2fs -f $Likk/Super/$TEN.img $(expr size_$TEN * 1024 + 200)M > /dev/null 2>&1; } 
-
-kichco() { size_$TEN="$(ls -l $Likk/Super/$TEN.img | awk '{print $5}')"; } 
-
-tongkichco() { super_size="$(ls -l $Likk/Super | sed -n 1p | awk '{print int($2)}')"; } 
-
-kichcosuper() { super_raw_size="$(awk "BEGIN {print int($ssize*1024*1024*1024)}")"; } 
+tangkichco() { resize2fs -f $Likk/Super/$TEN.img $(expr "$S${TEN}" * 1024 + 200)M > /dev/null 2>&1; } 
 
 cd $Likk/Super 
-for EXT in system.img vendor.img product.img system_ext.img odm.img; do 
-TEN=$(echo $EXT | awk -F. '{print $1}'); 
-kichco 
-[ -s $Likk/Super/$EXT ] && giamthieu
-done 
-
-for EXT in system.img vendor.img system_ext.img; do 
-TEN=$(echo $EXT | awk -F. '{print $1}'); 
-kichco 
-[ -s $Likk/Super/$EXT ] && tangkichco 
-done 
-
 tongkichco 
 kichcosuper 
 
-if [ "$super_size" -lt "$super_raw_size" ]; then 
+if [ "$Ssuper" -lt "$Ssuperr" ]; then 
 taosuper
 else 
-cd $Likk/Super
-for EXT in system.img vendor.img product.img system_ext.img odm.img; do 
-TEN=$(echo $EXT | awk -F. '{print $1}'); 
-kichco
-[ -s $Likk/Super/$EXT ] && giamthieu 
-done 
+for EXT in system.img vendor.img product.img system_ext.img odm.img; do TEN=$(echo $EXT | awk -F. '{print $1}'); kichco; [ -s $Likk/Super/$EXT ] && giamthieu; done 
+
+for EXT in system.img vendor.img system_ext.img; do TEN=$(echo $EXT | awk -F. '{print $1}'); kichco; [ -s $Likk/Super/$EXT ] && tangkichco; done 
+
 tongkichco 
 taosuper
 fi 
 
 # Phiên bản rom
-ten=$(grep 'incremental' $Likk/Unzip/*/*/*/metadata | awk -F= '{print $2}'); 
-mamay=$(grep 'pre-device' $Likk/Unzip/*/*/*/metadata | awk -F= '{print $2}'); 
+Ten=$(grep 'incremental' $Likk/Unzip/*/*/*/metadata | awk -F= '{print $2}'); 
+Mamay=$(grep 'pre-device' $Likk/Unzip/*/*/*/metadata | awk -F= '{print $2}'); 
 
 # Tạo zip flash 
-sed -i "s|Device:|Device: $mamay|; s|ROM: MIUI|ROM: MIUI $ten|" $Likk/Lib/Flash_2in1/*/*/*/update-binary 
+sed -i "s|Device:|Device: $Mamay|; s|ROM: MIUI|ROM: MIUI $Ten|" $Likk/Lib/Flash_2in1/*/*/*/update-binary 
 if [ -s $Likk/super.img ]; then 
 zstd -10 $Likk/super.img -o $Likk/Lib/images/super.img.zst 
 # rm -f $Likk/Payload/vbmeta.img vbmeta_system.img 2> /dev/null 
 mv -f $Likk/Payload/* $Likk/Lib/images 
 cd $Likk/Lib
 zip -9qr $Likk/Rom_2in1_${mamay}_${ten}.zip * 
-[ -s $Likk/Rom_2in1_${mamay}_${ten}.zip ] && echo " + Tạo xong"; 
+[ -s $Likk/Rom_2in1_${Mamay}_${Ten}.zip ] && echo " + Tạo xong"; 
 fi 
