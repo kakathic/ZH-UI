e75605b15c85a3496d9c1c1f06456fd778163608
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-25 02:25:51 +0700

    Update Unpack.sh

--- Unpack.sh
+++ Unpack.sh
@@ -1,9 +1,10 @@
-Xemb(){ ls -l $1 | awk '{print $5}'; }
+Xemb() { ls -l $1 | awk '{print $5}'; }
 
-echo "- Bung payload.bin"
+echo " + Trích xuất payload.bin"
 
 python3 $Likk/Libpy/payload_dumper.py $Likk/Unzip/payload.bin --out $Likk/Payload > /dev/null 
 
 for kdbfnd in system.img vendor.img product.img odm.img system_ext.img; do
 mv -f $Likk/Payload/$kdbfnd $Likk/Super
 done
+[ $? -eq 0 ] && echo " + Trích xuất xong!" 
