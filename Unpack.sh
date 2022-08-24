4b1ebf4662d381f3352bc98e0cded3f1bc6ea9cb
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-24 11:43:23 +0700

    Update Unpack.sh

--- Unpack.sh
+++ Unpack.sh
@@ -7,12 +7,3 @@ python3 $Likk/Libpy/payload_dumper.py $Likk/Unzip/payload.bin --out $Likk/Payloa
 for kdbfnd in system.img vendor.img product.img odm.img system_ext.img; do
 mv -f $Likk/Payload/$kdbfnd $Likk/Super
 done
-
-system_size=$(Xemb $Likk/Super/system.img)
-system_ext_size=$(Xemb $Likk/Super/system_ext.img)
-vendor_size=$(Xemb $Likk/Super/vendor.img)
-product_size=$(Xemb $Likk/Super/product.img)
-odm_size=$(Xemb $Likk/Super/odm.img)
-# Tính tổng kích cỡ thư mục super 
-super_size=$(ls -l $Likk/Super | sed -n 1p | awk '{print int($2)}'); 
-
