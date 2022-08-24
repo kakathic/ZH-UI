f5d7a2204f364fd408261c12307ecc70a7f7ae0c
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-24 12:12:03 +0700

    Update Repack.sh

--- Repack.sh
+++ Repack.sh
@@ -1,1 +1,30 @@
-echo 1
+# Tính kích cỡ từng tập tin 
+system_size=$(Xemb $Likk/Super/system.img)
+system_ext_size=$(Xemb $Likk/Super/system_ext.img)
+vendor_size=$(Xemb $Likk/Super/vendor.img)
+product_size=$(Xemb $Likk/Super/product.img)
+odm_size=$(Xemb $Likk/Super/odm.img)
+
+# Tính tổng kích cỡ thư mục super 
+super_size=$(ls -l $Likk/Super | sed -n 1p | awk '{print int($2)}'); 
+
+# Tính kích cỡ phân vùng super 8.5GB
+ssize=8.5
+super_raw_size=$(awk "BEGIN {print int($ssize*1024*1024*1024)}"); 
+
+# Loại phân vùng (1,2,3) 
+sokhe=1
+
+# Tên nhóm
+nhom=main
+
+# Chế độ (none/readonly)
+chedo=none
+
+# Tạo super.img
+lpmake -d "$super_raw_size" -s "$sokhe" -m 65536 -g "$nhom":"$super_size" --super-name super -p system:"$chedo":"$system_size":"$nhom" -i system=system.img -p system_ext:"$chedo":"$system_ext_size":"$nhom" -i system_ext=system_ext.img -p vendor:"$chedo":"$vendor_size":"$nhom" -i vendor=vendor.img -p product:"$chedo":"$product_size":"$nhom" -i product=product.img -p odm:"$chedo":"$odm_size":"$nhom" -i odm=odm.img -o $Likk/super.img
+
+# Cập nhật zip flash
+mkdir -p $Likk/tmp
+unzip -qo Flash_2in1.zip -d $Likk/tmp
+[ -s $Likk/super.img ] && mv -f $Likk/super.img $Likk/tmp/images 
