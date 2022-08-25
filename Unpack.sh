56d0f76612aac60ce81efa11c781454ce9928487
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-25 20:59:47 +0700

    Update Unpack.sh

--- Unpack.sh
+++ Unpack.sh
@@ -1,12 +1,28 @@
 Xemb() { ls -l $1 | awk '{print $5}'; }
 
-echo " + Trích xuất payload.bin"
-
+if [[ -s $Likk/Unzip/payload.bin ]]; then 
+echo " + Trích xuất payload.bin" 
 python3 $Likk/Lib/Libpy/payload_dumper.py $Likk/Unzip/payload.bin --out $Likk/Payload > /dev/null 
+for tim in system.img system_a.img system_b.img vendor.img vendor_a.img vendor_b.img product.img product_a.img product_b.img system_ext.img system_ext_a.img system_ext_b.img odm.img odm_a.img odm_b.img; do [[ -f $Likk/Payload/$tim ]] && mv -f $Likk/Payload/$tim $Likk/Super; done 
+[[ -n "$(ls $Likk/Super/*.img)" ]] && echo " + Trích xuất xong!"  
+fi 
 
-[ $? -eq 0 ] && echo " + Trích xuất xong!" 
+if [[ -s $Likk/Unzip/system.new.dat.br ]]; then 
+echo " + Trích xuất new.dat.br"
+cd $Likk/Unzip 
+for ext in system.new.dat.br vendor.new.dat.br product.new.dat.br system_ext.new.dat.br odm.new.dat.br; do 
+Ten=$(echo "$ext" | awk -F. '{print $1}'); 
+[[ -s $ext ]] && brotli -df $ext
+[[ -s $Ten.new.dat ]] && python3 $Likk/Lib/Libpy/sdat2img.py $Ten.transfer.list $Ten.new.dat $Likk/Super/$Ten.img 
+done 
+[[ -n "$(ls $Likk/Super/*.img)" ]] && echo " + Trích xuất xong!" 
+fi 
 
-#for tim in system.img vendor.img product.img odm.img system_ext.img; do mv -f $Likk/Payload/$tim $Likk/Super; done
-
-case $tim in system.img|system_a.img|system_b.img|vendor.img|vendor_a.img|vendor_b.img|product.img|product_a.img|product_b.img|system_ext.img|system_ext_a.img|system_ext_b.img|odm.img|odm_a.img|odm_b.img) [[ -f $Likk/Payload/$tim ]] && mv -f $Likk/Payload/$tim $Likk/Super;; esac  
-
+if [[ -s $Likk/Unzip/super.img ]]; then 
+echo " + Trích xuất super.img" 
+cd $Likk/Unzip 
+Hexf=$(hexdump -n 4 super.img | cut -c 8-12); 
+[[ $Hexf = "ff3a" ]] && mv -f super.img supers.img && simg2img supers.img super.img 
+python3 $Likk/Lib/Libpy/lpunpack.py super.img $Likk/Super > /dev/null 
+[[ -n "$(ls $Likk/Super/*.img)" ]] && echo " + Trích xuất xong!" 
+fi 
