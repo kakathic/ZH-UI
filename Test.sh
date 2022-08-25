ff48ae49694ade96c86ec07d235022fbeade6f6b
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-25 13:26:56 +0700

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,8 +1,15 @@
 echo "- Chạy thử nghiệm lệnh"
 echo
-echo "$PATH"
-echo
-help
+
+sudo apt-get update > /dev/null
+sudo apt-get install zstd binutils e2fsprogs erofs-utils > /dev/null
+pip3 install protobuf bsdiff4 six crypto construct google docopt pycryptodome > /dev/null
+
+echo "pycryptodome
+docopt
+protobuf<=3.20.1" > requirements.txt
+pip3 install -r requirements.txt > /dev/null
+
 lpmake 
 resize2fs
 e2fsck
