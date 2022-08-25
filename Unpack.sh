482c6109000bcd3a8898609707e5e930a89475dd
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-25 17:46:26 +0700

    Update Unpack.sh

--- Unpack.sh
+++ Unpack.sh
@@ -4,7 +4,7 @@ echo " + Trích xuất payload.bin"
 
 python3 $Likk/Lib/Libpy/payload_dumper.py $Likk/Unzip/payload.bin --out $Likk/Payload > /dev/null 
 
-for kdbfnd in system.img vendor.img product.img odm.img system_ext.img; do
-mv -f $Likk/Payload/$kdbfnd $Likk/Super
+for ten in system.img vendor.img product.img odm.img system_ext.img system_a.img system_ext_a.img vendor_a.img product_a.img odm_a.img system_b.img system_ext_b.img vendor_b.img product_b.img odm_b.img; do
+mv -f $Likk/Payload/$ten $Likk/Super
 done
 [ $? -eq 0 ] && echo " + Trích xuất xong!" 
