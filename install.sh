f0f24e9d04c99f47e0658bfc4f76a6d1f8b7bee2
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-26 09:32:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ Tải file bổ sung...
 "
 
 sudo apt-get update > /dev/null
-sudo apt-get install zstd binutils e2fsprogs erofs-utils simg2img > /dev/null
+sudo apt-get install zstd binutils e2fsprogs erofs-utils simg2img img2simg > /dev/null
 pip3 install protobuf bsdiff4 six crypto construct google docopt pycryptodome > /dev/null
 
 echo "pycryptodome
@@ -37,8 +37,8 @@ pip3 install -r requirements.txt > /dev/null
 # unzip -qo "$Likk/rom.zip" -d "$Likk/Unzip"
 # file "$Likk/rom.zip"
 
-Taive "$(Getpro Http)" "$Likk/rom.ext" 
-unzip -qo "$Likk/rom.ext" -d "$Likk/Unzip" || tar -xf "$Likk/rom.ext" -C "$Likk/Unzip"
+Taive "$(Getpro Http)" "$Likk/rom.zip" 
+[[ $(hexdump -n 4 "$Likk/rom.zip" | cut -c 8-12) == 4b50 ]] && unzip -qo "$Likk/rom.zip" -d "$Likk/Unzip" || tar -xf "$Likk/rom.zip" -C "$Likk/Unzip"
 [[ -s $Likk/Unzip/images/super.img ]] && mv -f $Likk/Unzip/images/super.img $Likk/Unzip/super.img
 
 echo "
