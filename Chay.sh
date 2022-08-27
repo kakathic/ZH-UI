7076c4c3a9a8da5a869c0182fa548b57883867c4
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-27 09:11:15 +0700

    Update Chay.sh

--- Chay.sh
+++ Chay.sh
@@ -25,7 +25,7 @@ done
 echo "- Tải tập tin"
 Taive() { curl -s -L "$1" -o "$2"; }
 Xem() { curl -s -L -G "$@"; }
-Laylink() { grep -m1 "$1" $Likk/Zom_custom.md; } 
+Laylink() { grep -m1 "$1" $Likk/Zom_custom.md | awk -F= '{print $2}'; } 
 
 echo "- Tìm tên rom" 
 Tenrom=$(grep -m1 "https://" $Likk/Zom_custom.md | awk -F/ '{print $5}') && Dinhdang=$(echo $Tenrom | awk -F. '{print $NF}');
@@ -36,9 +36,12 @@ Tên rom: $Tenrom
 echo " Link Rom: $(Laylink "https://")"
 echo " Tải về"
 Taive "$(Laylink "https://")" "$Likk/$Tenrom" 
-
+echo "- Giải nén"
+if [[ -s $Likk/$Tenrom ]]; then 
 echo "- Kiểm tra định dạng"
 [[ "$Dinhdang" == "zip" ]] && unzip -qo "$Likk/$Tenrom" -d "$Likk/Unzip"
 [[ "$Dinhdang" == "tgz" ]] && tar -cf "$Likk/$Tenrom" -C "$Likk/Unzip"
 [[ -s $Likk/Unzip/images/super.img ]] && mv -f $Likk/Unzip/images/super.img $Likk/Unzip/super.img 
+else "- Không có tập tin rom"
+fi 
 echo "- Xong"
