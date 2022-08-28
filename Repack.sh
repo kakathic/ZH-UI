58eb236fa963f349cbcbdab87d6d442b655c816b
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-28 19:27:50 +0700

    Update Repack.sh

--- Repack.sh
+++ Repack.sh
@@ -13,50 +13,6 @@ Chedo=none
 
 # Kích cỡ phân vùng super 8.5GB
 Ssize=8.5
-kichcosuper() { Ssuperr=$(echo $Ssize | awk '{print int($Ssize*1024*1024*1024)}'); } 
-kichcosuper 
-# Loại phân vùng 
-
-if [[ -f $Likk/Payload/modem.img ]] || [[ -f $Likk/Unzip/images/modem.img ]] || [[ -f $Likk/Unzip/firmware-update/modem.img ]]; then 
-
-for n in system_b.img system_ext_b.img vendor_b.img product_b.img odm_b.img; do touch $Likk/Super/$n 2> /dev/null; done 
-
-for i in system.img system_ext.img vendor.img product.img odm.img; do mv -f $Likk/Super/$i $Likk/Super/${i//./_a.} 2> /dev/null; done 
-
-Khe='_a'
-
-Sokhe=3
-
-Nhom='qti_dynamic_partitions_a'
-
-Nhomkhac='--group "qti_dynamic_partitions_b":"$Ssize" --partition system_b:"none":"0":"qti_dynamic_partitions_b" --image system_b=system_b.img --partition system_ext_b:"none":"0":"qti_dynamic_partitions_b" --image system_ext_b=system_ext_b.img --partition vendor_b:"none":"0":"qti_dynamic_partitions_b" --image vendor_b=vendor_b.img --partition product_b:"none":"0":"qti_dynamic_partitions_b" --image product_b=product_b.img --partition odm_b:"none":"0":"qti_dynamic_partitions_b" --image odm_b=odm_b.img' 
-
-else 
-
-Khe= 
-
-Sokhe=1
-
-Nhom="main" 
-
-Nhomkhac= 
-
-fi 
-
-# Loại phân vùng 
-if [[ -f $Likk/Payload/modem.img ]] || [[ -f $Likk/Unzip/images/modem.img ]] || [[ -f $Likk/Unzip/firmware-update/modem.img ]]; then 
-for n in system_b.img system_ext_b.img vendor_b.img product_b.img odm_b.img; do touch $Likk/Super/$n 2> /dev/null; done 
-for i in system.img system_ext.img vendor.img product.img odm.img; do mv -f $Likk/Super/$i $Likk/Super/${i//./_a.} 2> /dev/null; done 
-Khe='_a'
-Sokhe=3
-Nhom='qti_dynamic_partitions_a'
-Nhomkhac='--group "qti_dynamic_partitions_b":"0" --partition system_b:"none":"0":"qti_dynamic_partitions_b" --image system_b=system_b.img --partition system_ext_b:"none":"0":"qti_dynamic_partitions_b" --image system_ext_b=system_ext_b.img --partition vendor_b:"none":"0":"qti_dynamic_partitions_b" --image vendor_b=vendor_b.img --partition product_b:"none":"0":"qti_dynamic_partitions_b" --image product_b=product_b.img --partition odm_b:"none":"0":"qti_dynamic_partitions_b" --image odm_b=odm_b.img' 
-else 
-Khe= 
-Sokhe=1
-Nhom="main" 
-Nhomkhac= 
-fi 
 
 # Kiểm tra kích cỡ 
 kichcosuper() { Ssuperr=$(echo $Ssize | awk '{print int($Ssize*1024*1024*1024)}'); } 
@@ -65,6 +21,25 @@ kichco() { Ssystem=$(wc -c < $Likk/Super/system${Khe}.img); Ssystem_ext=$(wc -c 
 
 tongkichco() { Ssuper=$(ls -l $Likk/Super | awk '{Ssuper+=$5} END {print Ssuper}'); } 
 
+
+# Loại phân vùng 
+if [[ -f $Likk/Payload/modem.img ]] || [[ -f $Likk/Unzip/images/modem.img ]] || [[ -f $Likk/Unzip/firmware-update/modem.img ]]; then 
+for n in system_b.img system_ext_b.img vendor_b.img product_b.img odm_b.img; do touch $Likk/Super/$n 2> /dev/null; done 
+for i in system.img system_ext.img vendor.img product.img odm.img; do mv -f $Likk/Super/$i $Likk/Super/${i//./_a.} 2> /dev/null; done 
+Khe='_a'
+Sokhe=3
+Nhom='qti_dynamic_partitions_a'
+kichcosuper
+tongkichco
+Nhomkhac='--group "qti_dynamic_partitions_b":"$Ssuperr" --partition system_b:"none":"0":"qti_dynamic_partitions_b" --image system_b=system_b.img --partition system_ext_b:"none":"0":"qti_dynamic_partitions_b" --image system_ext_b=system_ext_b.img --partition vendor_b:"none":"0":"qti_dynamic_partitions_b" --image vendor_b=vendor_b.img --partition product_b:"none":"0":"qti_dynamic_partitions_b" --image product_b=product_b.img --partition odm_b:"none":"0":"qti_dynamic_partitions_b" --image odm_b=odm_b.img' 
+else 
+Khe= 
+Sokhe=1
+Nhom="main" 
+Nhomkhac= 
+fi 
+
+# Tùy chỉnh kích cỡ 
 giamthieu() { for EXT in system${Khe}.img vendor${Khe}.img product${Khe}.img system_ext${Khe}.img odm${Khe}.img; do [[ -s $Likk/Super/$EXT ]] && resize2fs -f -M $Likk/Super/$EXT > /dev/null 2>&1 && resize2fs -f -M $Likk/Super/$EXT > /dev/null 2>&1; done; } 
  
 tangkichco() { for EXT in system${Khe}.img vendor${Khe}.img product${Khe}.img system_ext${Khe}.img; do [[ -s $Likk/Super/$EXT ]] && Size=$(echo "$(wc -c < $Likk/Super/$EXT)" | awk '{print int($TEN*1024+200)}') && resize2fs -f $Likk/Super/$EXT ${Size}M > /dev/null 2>&1; done; } 
@@ -72,7 +47,7 @@ tangkichco() { for EXT in system${Khe}.img vendor${Khe}.img product${Khe}.img sy
 ghidoc() { for EXT in system${Khe}.img vendor${Khe}.img product${Khe}.img system_ext${Khe}.img odm${Khe}.img; do [[ -s $Likk/Super/$EXT ]] && e2fsck -y -E unshare_blocks $Likk/Super/$EXT > /dev/null 2>&1; done; } 
 
 echo " + Tạo super.img..." 
-taosuper() { lpmake --device-size "$Ssuperr" --metadata-slots "$Sokhe" --metadata-size 65536 --super-name super --group "$Nhom":"$Ssuper" --partition system${Khe}:"$Chedo":"$Ssystem":"$Nhom" --image system${Khe}=system${Khe}.img --partition system_ext${Khe}:"$Chedo":"$Ssystem_ext":"$Nhom" --image system_ext${Khe}=system_ext${Khe}.img --partition vendor${Khe}:"$Chedo":"$Svendor":"$Nhom" --image vendor${Khe}=vendor${Khe}.img --partition product${Khe}:"$Chedo":"$Sproduct":"$Nhom" --image product${Khe}=product${Khe}.img --partition odm${Khe}:"$Chedo":"$Sodm":"$Nhom" --image odm${Khe}=odm${Khe}.img $Nhomkhac -o $Likk/tmp/super.img > /dev/null 2>&1; } 
+taosuper() { lpmake --device-size "$Ssuperr" --metadata-slots "$Sokhe" --metadata-size 65536 --super-name super --group "$Nhom":"$Ssuperr" --partition system${Khe}:"$Chedo":"$Ssystem":"$Nhom" --image system${Khe}=system${Khe}.img --partition system_ext${Khe}:"$Chedo":"$Ssystem_ext":"$Nhom" --image system_ext${Khe}=system_ext${Khe}.img --partition vendor${Khe}:"$Chedo":"$Svendor":"$Nhom" --image vendor${Khe}=vendor${Khe}.img --partition product${Khe}:"$Chedo":"$Sproduct":"$Nhom" --image product${Khe}=product${Khe}.img --partition odm${Khe}:"$Chedo":"$Sodm":"$Nhom" --image odm${Khe}=odm${Khe}.img $Nhomkhac -o $Likk/tmp/super.img > /dev/null 2>&1; } 
 
 kichcosuper && echo " Kích cỡ phân vùng super: $Ssuperr" 
 giamthieu && tangkichco && ghidoc 
