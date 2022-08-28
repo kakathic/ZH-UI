559025363d26a3b339f55d0e71cdbe7517b453a1
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-28 14:46:06 +0700

    Update Repack.sh

--- Repack.sh
+++ Repack.sh
@@ -15,6 +15,34 @@ Chedo=none
 Ssize=8.5
 
 # Loại phân vùng 
+
+if [[ -f $Likk/Payload/modem.img ]] || [[ -f $Likk/Unzip/images/modem.img ]] || [[ -f $Likk/Unzip/firmware-update/modem.img ]]; then 
+
+for n in system_b.img system_ext_b.img vendor_b.img product_b.img odm_b.img; do touch $Likk/Super/$n 2> /dev/null; done 
+
+for i in system.img system_ext.img vendor.img product.img odm.img; do mv -f $Likk/Super/$i $Likk/Super/${i//./_a.} 2> /dev/null; done 
+
+Khe='_a'
+
+Sokhe=3
+
+Nhom='qti_dynamic_partitions_a'
+
+Nhomkhac='--group "qti_dynamic_partitions_b":"$Ssize" --partition system_b:"none":"0":"qti_dynamic_partitions_b" --image system_b=system_b.img --partition system_ext_b:"none":"0":"qti_dynamic_partitions_b" --image system_ext_b=system_ext_b.img --partition vendor_b:"none":"0":"qti_dynamic_partitions_b" --image vendor_b=vendor_b.img --partition product_b:"none":"0":"qti_dynamic_partitions_b" --image product_b=product_b.img --partition odm_b:"none":"0":"qti_dynamic_partitions_b" --image odm_b=odm_b.img' 
+
+else 
+
+Khe= 
+
+Sokhe=1
+
+Nhom="main" 
+
+Nhomkhac= 
+
+fi 
+
+# Loại phân vùng 
 if [[ -f $Likk/Payload/modem.img ]] || [[ -f $Likk/Unzip/images/modem.img ]] || [[ -f $Likk/Unzip/firmware-update/modem.img ]]; then 
 for n in system_b.img system_ext_b.img vendor_b.img product_b.img odm_b.img; do touch $Likk/Super/$n 2> /dev/null; done 
 for i in system.img system_ext.img vendor.img product.img odm.img; do mv -f $Likk/Super/$i $Likk/Super/${i//./_a.} 2> /dev/null; done 
