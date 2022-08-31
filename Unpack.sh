95d01240be7f9e9c8f285fafd40a3977e577d939
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-08-31 11:28:38 +0700

    Update Unpack.sh

--- Unpack.sh
+++ Unpack.sh
@@ -39,12 +39,12 @@ fi
 
 cd $Likk/Super 
 mkdir -p /s 2> /dev/null 
-mkdir -p $Likk/apk 2> /dev/null 
+mkdir -p $Likk/Apk 2> /dev/null 
 for m in system vendor system_ext product odm system_a vendor_a system_ext_a product_a odm_a; do 
 [[ -s $m.img ]] && mount -o loop,rw,sync $m.img /s
 if [[ -n "$(ls /s 2> /dev/null)" ]]; then
  for i in ThemeManager.apk miui.apk miuisystem.apk framework.jar framework-ext-res.apk framework-res.apk core-oj.jar miui-services.jar services.jar MiuiSystemUI.apk Settings.apk PackageInstaller.apk; do 
-  find /s -type -f -name "*$i" -exec cp -af "$1" $Likk/apk {} +;
+  find /s -type -f -name "*$i" -exec cp -af "$1" $Likk/Apk {} +;
  done
 fi
 done 
