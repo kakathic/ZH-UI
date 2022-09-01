cbb2a07fea2b496a973da2dce4494a79f3ea8e24
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-09-01 08:18:29 +0700

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -31,8 +31,9 @@ mount --help | tee 1.txt
 
 Likk="$GITHUB_WORKSPACE" 
 echo " Thử ngiệm"
-cd ../ 
 cd /mnt 
+mkdir -p /mnt/s
+ls /mnt
 echo " Tạo nơi chứa"
 mkdir -p $Likk/tmp
 mkdir -p $Likk/Apk
@@ -41,8 +42,8 @@ echo " Tạo ext4"
 make_ext4fs -J -T 0 -l 64M -a system -L test $Likk/tmp/test.img $Likk/Apk
 ls $Likk/tmp/test.img 
 echo "Mount tập tin"
-mount -w $Likk/tmp/test.img $Likk/s
+mount -w $Likk/tmp/test.img /mnt/s
 sync 
 echo "Kiểm tra thư mục"
-ls $Likk/s
+ls /mnt/s
 echo " Kết thúc"
