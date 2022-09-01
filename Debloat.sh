e8659a27690e8fa3c8284628532dbe94430c4ee8
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-09-01 10:06:42 +0700

    Update and rename Repack.sh to Debloat.sh

--- Debloat.sh
+++ Debloat.sh
@@ -1,5 +1,5 @@
 
-danhsachxoa="
+Danhsachxoa="
 BaiduIME
 MIFinance
 MIUIDriveMode
@@ -192,16 +192,56 @@ MIUIDeskClockS
 #MIUITouchAssistant
 RideModeAudio
 "
-xoataptin() {
+Xoataptin() {
 Nha=/mnt
 cd /mnt
-for thumuc in s s/system s/system/system_ext s/system/product; do 
-if [[ -e $Nha/$thumuc ]]; then cd $Nha/$thumuc; 
-for ten in data-app app priv-app; do 
-[[ -e $Nha/$thumuc/$ten ]] && cd $Nha/$thumuc/$ten && for tim in $danhsach; do [[ -e $tim ]] && rm -rf $tim; done
+for Thumuc in s s/system s/system/system_ext s/system/product; do 
+if [[ -e $Nha/$Thumuc ]]; then cd $Nha/$Thumuc; 
+for Ten in data-app app priv-app; do 
+[[ -e $Nha/$Thumuc/$Ten ]] && cd $Nha/$Thumuc/$Ten && for Tim in $Danhsachxoa; do [[ -e $Tim ]] && rm -rf $Tim; done
 done 
 fi
 done 
 rm -rf $Nha/*recovery* $Nha/s/system/*/*auto-install*.json $Nha/s/system/media/theme/dynamicicons $Nha/s/system/media/theme/miui_mod_icons 2> /dev/null
 }
-xoataptin 
+ 
+Phanquyen() {
+find $(pwd) -type f -exec chmod -R 644 "$1" {} +;
+find $(pwd) -type d -exec chmod -R 755 "$1" {} +;
+find $(pwd) -type d -name "*app" -exec chmod -R 755 "$1" {} +;
+find $(pwd) -type f -name "*.apk" -exec chmod 644 "$1" {} +; 
+find $(pwd) -type f -name "*.jar" -exec chmod 644 "$1" {} +;
+find $(pwd) -type f -name "*.prop" -exec chmod 600 "$1" {} +;
+}
+
+Cheptaptin() {
+if [[ "$M" == "system" ]] || [[ "$M" == "system_a" ]]; then 
+  if [[ -n "$(ls /mnt/s 2> /dev/null)" ]] && [[ -n "$(ls $Likk/Mod)" ]]; then
+  cp -af $Likk/Mod/*ThemeManager.apk /mnt/s/system/app/MIUIThemeManager 2> /dev/null
+  cp -af $Likk/Mod/miui.apk /mnt/s/system/app/miui 2> /dev/null 
+  cp -af $Likk/Mod/miuisystem.apk /mnt/s/system/app/miuisystem 2> /dev/null 
+  cp -af $Likk/Mod/framework.jar /mnt/s/system/framework 2> /dev/null
+  cp -af $Likk/Mod/framework-ext-res.apk /mnt/s/system/framework/framework-ext-res 2> /dev/null
+  cp -af $Likk/Mod/framework-res.apk /mnt/s/system/framework 2> /dev/null
+  cp -af $Likk/Mod/services.jar /mnt/s/system/framework 2> /dev/null
+  cp -af $Likk/Mod/M*PackageInstaller.apk /mnt/s/system/priv-app/MIUIPackageInstaller 2> /dev/null
+  cp -af $Likk/Mod/core-oj.jar /mnt/s/system/framework 2> /dev/null 
+  fi
+ fi 
+ if [[ "$M" == "system_ext" ]] || [[ "$M" == "system_ext_a" ]]; then 
+  if [[ -n "$(ls /mnt/s 2> /dev/null)" ]] && [[ -n "$(ls $Likk/Mod)" ]]; then
+  cp -af $Likk/Mod/Settings.apk /mnt/s/priv-app/Settings 2> /dev/null
+  cp -af $Likk/Mod/MiuiSystemUI.apk /mnt/s/priv-app/MiuiSystemUI 2> /dev/null 
+  fi
+ fi 
+} 
+
+cd $Likk/Super 
+sudo mkdir -p /mnt/s 2> /dev/null 
+for M in system vendor system_ext product odm system_a vendor_a system_ext_a product_a odm_a; do 
+ [[ -s $M.img ]] && sudo mount -o rw,loop,sync $M.img /mnt/s && cd /mnt/s 
+ Cheptaptin; 
+ Xoataptin; 
+ Phanquyen; 
+ umount /mnt/s 2> /dev/null 
+done 
