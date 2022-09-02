0a97a3f26c98b1fa2738d2e13fa325d2932c9d1c
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-02 08:16:18 +0700

    Update Mod.sh

--- .github/Mod.sh
+++ .github/Mod.sh
@@ -1,3 +1,3 @@
-# Thư mục chứa tập tin: $Likk/Apk
+# Thư mục chứa tập tin: $TOME/Apk
 [[ -z "$(ls $TOME/Apk)" ]] && echo "- Không có tập tin nào!"  
-# Thư mục chứa apk,jar đã mod: $Likk/Mod
+# Thư mục chứa apk,jar đã mod: $TOME/Mod
