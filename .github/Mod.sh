0013b5630a68f35a93470e75f27a83a8a5730cbd
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-03 20:54:25 +0700

    Update Mod.sh

--- .github/Mod.sh
+++ .github/Mod.sh
@@ -6,5 +6,21 @@ apktool(){ java -Xmx512M -Dfile.encoding=utf-8 -jar $TOME/.github/Tools/kikfox.j
 baksmali(){ java -Xmx512M -Dfile.encoding=utf-8 -jar $TOME/.github/Tools/baksmali-2.3.4.jar "$@"; }
 smali(){ java -Xmx512M -Dfile.encoding=utf-8 -jar $TOME/.github/Tools/smali-2.5.2.jar "$@"; }
 
+Timkiem () { grep -Rl "$1" $2; }
+
+Thaythe () {
+Tt1="$(Timkiem "$1" "$3")"
+[ "$Tt1" ] && echo "MOD: $1 -> $2" | sed "s|\\\/|/|g"
+for Tt2 in $Tt1; do
+[ "$Tt2" ] && sed -i -e "s|$1|$2|g" $Tt2
+done
+}
+
+Vsmali () {
+for Vka in $(Timkiem "$1" "$4"); do
+echo "MOD: $(echo "$1" | sed 's|\\||g')"
+sed -i -e "/^$1/,/$2/c $(echo "$3" | sed -z 's|\n|\\n|g')" "$Vka"
+done
+}
 
 
