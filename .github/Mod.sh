2819e7a1f6390598e48838fe51d0106452baa946
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-03 20:29:07 +0700

    Update Mod.sh

--- .github/Mod.sh
+++ .github/Mod.sh
@@ -1,3 +1,10 @@
 # Thư mục chứa tập tin: $TOME/Apk
 [[ -z "$(ls $TOME/Apk)" ]] && echo "- Không có tập tin nào!"  
 # Thư mục chứa apk,jar đã mod: $TOME/Mod
+
+apktool(){ java -Xmx512M -Dfile.encoding=utf-8 -jar $TOME/.github/Tools/kikfox.jar "$@"; }
+baksmali(){ java -Xmx512M -Dfile.encoding=utf-8 -jar $TOME/.github/Tools/baksmali-2.3.2.jar "$@"; }
+smali(){ java -Xmx512M -Dfile.encoding=utf-8 -jar $TOME/.github/Tools/smali-2.5.2.jar "$@"; }
+
+
+
