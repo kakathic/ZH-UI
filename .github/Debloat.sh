b2e8e562cb072064e1f61c6a5ac84098f93465d9
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-09-04 08:46:59 +0700

    Update and rename Unpack.sh to Debloat.sh

--- .github/Debloat.sh
+++ .github/Debloat.sh
@@ -247,8 +247,7 @@ for Ten in $Phanvung; do
   e2fsck -fy $TOME/Super/$Ten.img
   [[ ! -e $Tam ]] && sudo mkdir -p $Tam
   [[ -n "$(ls $Tam)" ]] && sudo umount $Tam 
-  sudo losetup /dev/loop3
-  [[ -z "$(ls $Tam)" ]] && sudo mount -t ext4 -o rw,loop $TOME/Super/$Ten.img $Tam
+  [[ -z "$(ls $Tam)" ]] && sudo mount -t auto -o rw $TOME/Super/$Ten.img $Tam 2> $TOME/tmp/t && loop=$(cat $TOME/tmp/t | awk -F' '{print $2}') && [[ -n "$loop" ]] && sudo mount $loop $Tam 
   cd $Tam 
 echo "Chép"
   Cheptaptin 
