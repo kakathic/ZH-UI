82e712a0a363dd3ac16a54fae88b22d76af3b3c5
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-09-05 05:40:02 +0700

    Update Debloat.sh

--- .github/Debloat.sh
+++ .github/Debloat.sh
@@ -265,8 +265,8 @@ Thaydoi() {
 Chinhsua() {
    Size=$(wc -c < $TOME/Super/$Ten.img)
    SizeM=$(du -m $TOME/Super/$Ten.img | awk '{print int($S1+200)}')M
-   resize2fs -f $TOME/Super/$Ten.img ${SizeM} 
-   sudo mount -o rw,loop $TOME/Super/$Ten.img $New
+   #resize2fs -f $TOME/Super/$Ten.img ${SizeM} 
+   sudo mount -o ro,loop $TOME/Super/$Ten.img $New
    ls $New
    cd $New
    echo "Chép"
