0f2a4b16c6749ed2c757b37b16a1a00bbd195300
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-11 20:18:37 +0700

    Update Mod.sh

--- .github/Mod.sh
+++ .github/Mod.sh
@@ -19,9 +19,7 @@ Vsmali() {
 for Vka in $(Timkiem "$1" "$4"); do
 echo "MOD: $(echo "$1" | sed 's|\\||g')"
 sed -i -e "/^$1/,/$2/c $(echo "$3" | sed -z 's|\n|\\n|g')" "$Vka"
-echo "$Vak" >> $TOME/Mod/$(echo "$4" | sed "s|$TOME/Mod/||g" | cut -d '/' -f1)/class
-echo "$TOME/Mod/$(echo "$4" | sed "s|$TOME/Mod/||g" | cut -d '/' -f1)/class"
-
+echo "$Vka" >> $TOME/Mod/$(echo "$4" | sed "s|$TOME/Mod/||g" | cut -d '/' -f1)/class
 done
 }
 
