475effe5768cc5e7ec645edf82e229cf0ae45359
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-09-28 13:33:25 +0700

    Update Test.sh

--- .github/Test.sh
+++ .github/Test.sh
@@ -15,9 +15,12 @@ touch $New/test.txt
 
   $make_ext4fs -J -T 1230768000 -a / -L / -l $SizeM $TOME/tmp/$Ten.img $New/ 
   tune2fs -l $TOME/tmp/$Ten.img 
-  $e2fsdroid -T 1230768000 -a / -f $New/ $TOME/tmp/$Ten.img
+
+  $e2fsdroid -T 1230768000 $TOME/tmp/$Ten.img
   tune2fs -l $TOME/tmp/$Ten.img
+
   tune2fs -o +acl,+user_xattr -L / -M / -E hash_alg=half_md4 -O dir_index,filetype,extent,sparse_super,large_file,huge_file,uninit_bg,dir_nlink,extra_isize -e continue $TOME/tmp/$Ten.img
+
   #e2fsck -fy $TOME/tmp/$Ten.img 
   sudo mount -o rw,loop $TOME/tmp/$Ten.img $Tam
   ls -l $Tam
