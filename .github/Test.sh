435ae07820898ffea7cb0d69c78e768f7da0e23f
Chấm <111698740+chamchamfyy@users.noreply.github.com>
2022-09-28 11:40:18 +0700

    Update Test.sh

--- .github/Test.sh
+++ .github/Test.sh
@@ -13,13 +13,13 @@ Tenfc=$TOME/.github/${Ten}_file_contexts
 SizeM=$((4*1024))M
 touch $New/test.txt
 
-  $make_ext4fs -J -T 1230768000 -a / -L / -M / -l $SizeM $TOME/tmp/$Ten.img $New/ 
+  $make_ext4fs -J -T 1230768000 -a / -L / -l $SizeM -X $Tenfc $TOME/tmp/$Ten.img $New/ 
   tune2fs -l $TOME/tmp/$Ten.img 
   #exec $e2fsdroid -T 1230768000 -a / -f $New/ $TOME/tmp/$Ten.img
   #tune2fs -l $TOME/tmp/$Ten.img
-  tune2fs -o +acl -o +user_xattr -L '/' -M '/' $TOME/tmp/$Ten.img
+  tune2fs -o +acl,+user_xattr -L '/' -M '/' $TOME/tmp/$Ten.img
   #e2fsck -fy $TOME/tmp/$Ten.img 
-  #tune2fs -E hash_alg=half_md4 -O dir_index,filetype,extent,sparse_super,large_file,huge_file,uninit_bg,dir_nlink,extra_isize -e continue -o +user_xattr $TOME/tmp/$Ten.img 
+  #tune2fs -E hash_alg=half_md4 -O dir_index,filetype,extent,sparse_super,large_file,huge_file,uninit_bg,dir_nlink,extra_isize -e continue $TOME/tmp/$Ten.img 
   sudo mount -o rw,loop $TOME/tmp/$Ten.img $Tam
   ls -l $Tam
   #cd $TOME
