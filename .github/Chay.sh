8c0ad8496b07aedcfd5f6365c49bf7413ad66674
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-10-10 18:17:02 +0700

    Update Chay.sh

--- .github/Chay.sh
+++ .github/Chay.sh
@@ -1,5 +1,10 @@
 #kakathic & chamchamfy
 
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /opt/ghc
+sudo rm -rf "/usr/local/share/boost"
+sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+
 export TOME="$GITHUB_WORKSPACE"
 export PATH="$TOME/.github/bin:$PATH"
 chmod -R 777 .github/bin/* >/dev/null
